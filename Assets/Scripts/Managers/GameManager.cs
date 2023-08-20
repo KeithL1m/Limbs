791640b35071da43c7415ad0edfc1c1512295349
KeithLim <85144995+KeithL1m@users.noreply.github.com>
2023-08-20 01:52:09 -0700

    System Manager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Manager
 {
     public List<PlayerInput> playerList = new List<PlayerInput>();
 
