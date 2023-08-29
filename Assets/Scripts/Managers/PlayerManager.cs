4c488b700ebf239ced78d8f5816cba21d4610dde
KeithLim <85144995+KeithL1m@users.noreply.github.com>
2023-08-29 15:49:11 -0700

    arrow indicator start and player manager

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class PlayerManager : Manager
 {
     [SerializeField] Player _basePlayer;
+    public GameObject arrowIndicatorPrefab;
     List<Player> _playerList = new List<Player>();
     public Player this[int playerNumber] { get { return _playerList[playerNumber]; } }
     
     public void AddPlayer(Player player)
     {
         _playerList.Add(player);
+    }
+
+    void Start()
+    {
+        // Instantiate arrow indicator and attach it to the player.
+        GameObject arrowIndicator = Instantiate(arrowIndicatorPrefab, _basePlayer.transform.position, Quaternion.identity);
+        arrowIndicator.transform.SetParent(_basePlayer.transform);
     }
 
     void Update()
