5a4a33204fd942b5c8cd5f316994fe4d91c7629e
KeithLim <85144995+KeithL1m@users.noreply.github.com>
2023-10-11 22:08:54 -0700

    Scripts for Pinata

--- Assets/Scripts/Pinata.cs
+++ Assets/Scripts/Pinata.cs
@@ -6,13 +6,31 @@ public class Pinata : MonoBehaviour
 {
     [SerializeField]
     private float _maxHealth;
-    [SerializeField]
+    private float _health;
 
-    public float _health;
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        _health = _maxHealth;    
+    }
+    // Update is called once per frame
+
+    //Dealing damage to Pinata
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Limb")
+        {
+            _health -= 10.0f;
+        }
+
+        if(_health <= 0.0f)
+        {
+            PinataDestroyed();
+        }
+    }
+
+    void PinataDestroyed()
+    {
+            MapManager.instance.LoadMap();
     }
 }
