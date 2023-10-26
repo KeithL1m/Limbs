97920e460db89f4fd6c40a6d96649f43a494241d
KeithL <85144995+KeithL1m@users.noreply.github.com>
2023-10-26 14:56:02 -0700

    Camera updates

--- Assets/Scripts/CameraScripts/CameraManager.cs
+++ Assets/Scripts/CameraScripts/CameraManager.cs
@@ -10,6 +10,7 @@ public class CameraManager : MonoBehaviour
     public CameraFocus _focusLevel;
 
     public List<GameObject> _players;
+    public List<PlayerInput> _playerList = new List<PlayerInput>();
 
     public float depthUpdateSpeed = 5.0f;
     public float angleUpdateSpeed = 7.0f;
@@ -42,6 +43,25 @@ public class CameraManager : MonoBehaviour
 
     private void CalculateCameraLocation()
     {
+        Vector3 averageCenter = Vector3.zero;
+        Vector3 totalPositions = Vector3.zero;
+        Bounds playerBounds = new Bounds();
 
+        for (int i = 0; i < _playerList.Count; i++)
+        {
+            Vector3 playerPosition = _playerList[i].transform.position;
+
+            if(!_focusLevel.focusBounds.Contains(playerPosition))
+            {
+                float playerX = Mathf.Clamp(playerPosition.x, _focusLevel.focusBounds.min.x, _focusLevel.focusBounds.max.x);
+                float playerY = Mathf.Clamp(playerPosition.y, _focusLevel.focusBounds.min.y, _focusLevel.focusBounds.max.y);
+                float playerZ = Mathf.Clamp(playerPosition.z, _focusLevel.focusBounds.min.z, _focusLevel.focusBounds.max.z);
+            }
+
+            totalPositions += playerPosition;
+            playerBounds.Encapsulate(playerPosition);
+        }
+
+        averageCenter = (totalPositions / _playerList.Count);
     }
 }
