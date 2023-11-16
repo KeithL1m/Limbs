acf875371cfe70dd374315dc23a9d56369b1b3b3
diegobatiz <diegow.batiz@gmail.com>
2023-11-16 12:52:38 -0800

    game fixes

--- Assets/Scripts/Managers/MapManager.cs
+++ Assets/Scripts/Managers/MapManager.cs
@@ -29,12 +29,12 @@ public class MapManager : Manager
     public void LoadMap()
     {
         #if LIMBS_DEBUG
-        var debugSceneName = ServiceLocator.Get<DebugSettings>().NextScene;
-        if (string.IsNullOrWhiteSpace(debugSceneName) == false)
-        {
-            SceneManager.LoadScene(debugSceneName);
-            return;
-        }
+        //var debugSceneName = ServiceLocator.Get<DebugSettings>().NextScene;
+        //if (string.IsNullOrWhiteSpace(debugSceneName) == false)
+        //{
+        //    SceneManager.LoadScene(debugSceneName);
+        //   return;
+        //}
         #endif
 
         if (_gm.VictoryScreen)
