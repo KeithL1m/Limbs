19354b2dfbda0077edcf6c5d557b7f78728a20e1
KeithL <85144995+KeithL1m@users.noreply.github.com>
2023-11-20 19:32:55 -0800

    DebugSettingFixed??

--- Assets/Scripts/Managers/MapManager.cs
+++ Assets/Scripts/Managers/MapManager.cs
@@ -52,7 +52,7 @@ public class MapManager : Manager
     {
         if (_gm.startScreen)
             return;
-        Debug.Log("new scene loaded");
+        Debug.Log("New scene loaded");
         _gm.OnStart();
     }
 }
