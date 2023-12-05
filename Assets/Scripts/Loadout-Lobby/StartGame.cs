d0e2d73f787cbce988162de04f5bf808707650c8
diegobatiz <diegow.batiz@gmail.com>
2023-12-04 20:05:30 -0800

    don't delete the cucumber

--- Assets/Scripts/Loadout-Lobby/StartGame.cs
+++ Assets/Scripts/Loadout-Lobby/StartGame.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
@@ -8,6 +9,9 @@ public class StartGame : MonoBehaviour
     private ConfigurationManager _configManager;
 
     public GameObject SelectedButton;
+
+    [SerializeField] private Texture2D _textureToSave;
+    private string _filePath;
 
     public int nextScene;
 
@@ -21,6 +25,26 @@ public class StartGame : MonoBehaviour
     {
         _configManager = ServiceLocator.Get<ConfigurationManager>();
         EventSystem.current.SetSelectedGameObject(SelectedButton);
+
+        if (!Directory.Exists(Application.persistentDataPath + "/DO_NOT_DELETE/"))
+        {
+            Directory.CreateDirectory(Application.persistentDataPath + "/DO_NOT_DELETE/");
+
+            _filePath = Path.Combine(Application.persistentDataPath + "/DO_NOT_DELETE/", "NEEDED_FILE_DO_NOT_DELETE.png");
+            
+            byte[] pngData = _textureToSave.EncodeToPNG();
+
+            File.WriteAllBytes(_filePath, pngData);
+
+            Debug.Log("Sprite saved as PNG at: " + _filePath);
+
+            Debug.Log("Directory Created");
+        }
+
+        if (!File.Exists(_filePath))
+        {
+            Application.Quit();
+        }
     }
 
     public void LoadGame()
