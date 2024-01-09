31a0fb900157108b52b70e4245df032375a73224
KebinDev <134655367+KebinDev@users.noreply.github.com>
2024-01-08 20:07:57 -0800

    Sticky platform and snowball/icicle added

---
+++ Assets/Scripts/LevelSpecific/StickyPlatform.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyPlatform : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private RigidbodyConstraints2D originalRBC;
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            rb = collision.collider.GetComponent<Rigidbody2D>();
+            originalRBC = rb.constraints;
+            rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
+
+            StartCoroutine(UnstickPlayer());
+            Debug.Log("STUCK");
+        }
+
+        else
+        {
+            Debug.Log("No");
+            StartCoroutine(UnstickPlayer());
+        }
+    }
+
+    private IEnumerator UnstickPlayer()
+    {
+        float unstickTime = Time.realtimeSinceStartup + 5.0f;
+
+        while (Time.realtimeSinceStartup < unstickTime)
+        {
+            yield return null;
+        }
+            Debug.Log("Done sticking!");
+
+        rb.constraints = originalRBC;
+    }
+
+}
