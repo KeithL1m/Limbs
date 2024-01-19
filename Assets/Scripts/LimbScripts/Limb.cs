f1ac0f264cb4eed1f398fa869cbd9856e102e20f
KeithLim <85144995+KeithL1m@users.noreply.github.com>
2024-01-19 08:38:55 -0800

    change limb scripts infos from private to protected

--- Assets/Scripts/LimbScripts/Limb.cs
+++ Assets/Scripts/LimbScripts/Limb.cs
@@ -29,7 +29,7 @@ public class Limb : MonoBehaviour
     [HideInInspector] public LimbType Type { get; set; } //this will help most with animations
     [HideInInspector] public LimbState State { get; set; }
 
-    [SerializeField] private LimbData _limbData;
+    [SerializeField] protected LimbData _limbData;
     [field: SerializeField] public GameObject Trail { get; set; }
     [field: SerializeField]  public GameObject PickUpIndicator { get; set; }
 
@@ -39,12 +39,12 @@ public class Limb : MonoBehaviour
 
     //limb properties
     public float Size { get; set; }
-    private Vector2 _throwVelocity; //used when not aiming
-    private float _throwSpeed; //used when aiming
-    private float _damage;
-    private float _specialDamage;
-    private Vector3 _returnVelocity;
-    private float _rVMultiplier;
+    protected Vector2 _throwVelocity; //used when not aiming
+    protected float _throwSpeed; //used when aiming
+    protected float _damage;
+    protected float _specialDamage;
+    protected Vector3 _returnVelocity;
+    protected float _rVMultiplier;
 
     private void Awake()
     {
