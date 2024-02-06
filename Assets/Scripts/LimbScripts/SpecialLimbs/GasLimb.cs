20d84a68261d4c2f017f4eaa97e3bc9957bd38dc
KeithL <85144995+KeithL1m@users.noreply.github.com>
2024-02-05 21:27:00 -0800

    gas fixes

--- Assets/Scripts/LimbScripts/SpecialLimbs/GasLimb.cs
+++ Assets/Scripts/LimbScripts/SpecialLimbs/GasLimb.cs
@@ -1,14 +1,45 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine;
 
 public class GasLimb : Limb
 {
     private ParticleManager _particleManager;
-    
+    [SerializeField] Collider2D _collider;
+    [SerializeField] float _delayTimer = 0.2f;
+
+    public GameObject gasCloudPrefab;
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
     protected override void Initialize()
     {
         base.Initialize();
         _particleManager = ServiceLocator.Get<ParticleManager>();
         _specialLimbs = true;
+    }
+
+    public override void ThrowLimb(int direction)
+    {
+        base.ThrowLimb(direction);
+
+        StartCoroutine(EnableAfterDelay());
+    }
+
+    private IEnumerator EnableAfterDelay()
+    {
+        _collider.enabled = false;
+        yield return new WaitForSeconds(_delayTimer);
+        _collider.enabled = true;
+        yield break;
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D collision)
+    {
+        base.OnTriggerEnter2D(collision);
     }
 
     protected override void OnCollisionEnter2D(Collision2D collision)
@@ -18,6 +49,7 @@ public class GasLimb : Limb
 
         GasExplode();
     }
+
 
     void GasExplode()
     {
