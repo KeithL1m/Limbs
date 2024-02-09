039180202553d7475b70b0a03d87039ca255a1f6
diegobatiz <diegow.batiz@gmail.com>
2024-02-09 11:53:19 -0800

    clash particles

--- Assets/Scripts/Managers/ParticleManager.cs
+++ Assets/Scripts/Managers/ParticleManager.cs
@@ -5,6 +5,7 @@ public class ParticleManager
     private const string GasPoolName = "GasBomb";
     private const string ExplosionName = "Explosion";
     private const string TeleportName = "Teleport";
+    private const string ClashName = "Clash";
     
     private ObjectPoolManager _objectPoolManager;
     
@@ -31,7 +32,18 @@ public class ParticleManager
     public void PlayTeleportParticle(Vector3 pos) 
     {
         var gas = _objectPoolManager.GetObjectFromPool(TeleportName);
-        gas.transform.position.Set(pos.x, pos.y, pos.z);
+        gas.transform.position = new Vector3(pos.x, pos.y, pos.z);
         gas.SetActive(true);
+    }
+
+    public void PlaySwordClashParticle(Vector3 pos)
+    {
+        GameObject sword = _objectPoolManager.GetObjectFromPool(ClashName);
+        Debug.Log(pos);
+        Debug.Log(sword.transform.position);
+        sword.transform.position = new Vector3(pos.x, pos.y, pos.z);
+        Debug.Log(pos);
+        Debug.Log(sword.transform.position);
+        sword.SetActive(true);
     }
 }
