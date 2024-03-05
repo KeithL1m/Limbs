c126904123fe5f3149453f736b76c7fe8a24dff4
KeithL <85144995+KeithL1m@users.noreply.github.com>
2024-03-04 19:03:33 -0800

    update force for Chainsaw

--- Assets/Scripts/LevelSpecific/Sawblade.cs
+++ Assets/Scripts/LevelSpecific/Sawblade.cs
@@ -39,14 +39,13 @@ public class Sawblade : MonoBehaviour
         transform.Rotate(0.0f, 0.0f, _rotateSpeed * Time.deltaTime);
 
         // Check if chainsaw slows down
-        if(_sawChain.GetComponent<Rigidbody2D>().velocity.magnitude < 10.0f)
+        if(_sawChain.GetComponent<Rigidbody2D>().velocity.magnitude < 15.0f)
         {
-            _sawChain.AddForce(new Vector2(100.0f, 0.0f));
+            _sawChain.AddForce(new Vector2(200.0f, 0.0f));
         }
-
-        // Do damage to player
         
     }
+    // Do damage to player
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
