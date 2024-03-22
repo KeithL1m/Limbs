a53ff65ed015d29b77a6769e0f1aebb4bafee733
WZXiaoZZ <wangzixiao1999@outlook.com>
2024-03-22 08:38:22 +0800

    Change StickBomb Particle

--- Assets/Scripts/Managers/ParticleManager.cs
+++ Assets/Scripts/Managers/ParticleManager.cs
@@ -14,10 +14,11 @@ public class ParticleManager
     public const string DeathName = "Death";
     public const string DrunkLiquidBubbleBurstName = "DrunkLiquidBubbleBurst";
     public const string DrunkRisingBubbleName = "DrunkRisingBubble";
+    public const string StickBombName = "StickBomb";
 
 
     private ObjectPoolManager _objectPoolManager;
-    
+
     public ParticleManager Initialize()
     {
         _objectPoolManager = ServiceLocator.Get<ObjectPoolManager>();
@@ -33,7 +34,7 @@ public class ParticleManager
         gas.SetActive(true);
         gasParticle.SetActive(true);
     }
-    
+
     public void PlayExplosionParticle(Vector3 pos)
     {
         var gas = _objectPoolManager.GetObjectFromPool(ExplosionName);
@@ -47,13 +48,29 @@ public class ParticleManager
         gas.transform.position = new Vector3(pos.x, pos.y, pos.z);
         gas.SetActive(true);
     }
-    
 
-    public void PlayTeleportParticle(Vector3 pos) 
+
+    public void PlayParticle(string name, Vector2 pos)
+    {
+        var particle = _objectPoolManager.GetObjectFromPool(name);
+        particle.transform.position = new Vector3(pos.x, pos.y);
+        particle.transform.position = new Vector3(pos.x, pos.y);
+        particle.SetActive(true);
+    }
+
+
+    public void PlayTeleportParticle(Vector3 pos)
     {
         var gas = _objectPoolManager.GetObjectFromPool(TeleportName);
         gas.transform.position = new Vector3(pos.x, pos.y, pos.z);
         gas.SetActive(true);
+    }
+
+    public void PlayStickBombParticle(Vector3 pos)
+    {
+        var stickBomb= _objectPoolManager.GetObjectFromPool(StickBombName);
+        stickBomb.transform.position = new Vector3(pos.x, pos.y, pos.z);
+        stickBomb.SetActive(true);
     }
 
     public void PlaySwordClashParticle(Vector3 pos)
@@ -68,7 +85,7 @@ public class ParticleManager
         redExplosion.transform.position = new Vector3(pos.x, pos.y, pos.z);
         redExplosion.SetActive(true);
     }
-    public void PlayConfettiParticle(Vector3 pos) 
+    public void PlayConfettiParticle(Vector3 pos)
     {
         GameObject confetti = _objectPoolManager.GetObjectFromPool(ConfettiName);
         confetti.transform.position = new Vector3(pos.x, pos.y, pos.z);
@@ -88,7 +105,7 @@ public class ParticleManager
     }
     public void PlayDeathParticle(Vector3 pos)
     {
-        GameObject death= _objectPoolManager.GetObjectFromPool(DeathName);
+        GameObject death = _objectPoolManager.GetObjectFromPool(DeathName);
         death.transform.position = new Vector3(pos.x, pos.y, pos.z);
         death.SetActive(true);
     }
