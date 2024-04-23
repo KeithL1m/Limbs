7f01d8d6a94c9262c8cda91b107a94a47a4d0b9c
diegobatiz <diegow.batiz@gmail.com>
2024-04-23 16:57:21 -0700

    Update Arsenal.cs

--- Assets/Scripts/UI/Arsenal.cs
+++ Assets/Scripts/UI/Arsenal.cs
@@ -7,12 +7,13 @@ public class Arsenal : MonoBehaviour
     [SerializeField] private List<GameObject> _limbList;
     [SerializeField] private int _maxAmount;
     [SerializeField] private float _spawnTime = 3f;
+    [SerializeField] private float _spawnTimeRange = 1f;
 
     public void Setup()
     {
         var limbManager = ServiceLocator.Get<LimbManager>();
         limbManager.SetLimbOptions(_limbList);
         limbManager.SetMaxAmount(_maxAmount);
-        limbManager.SetSpawnTime(_spawnTime);
+        limbManager.SetSpawnTime(_spawnTime, _spawnTimeRange);
     }
 }
