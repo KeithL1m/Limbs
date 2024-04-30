86d3edc9ef9f91794d1935f02f40cab8c82cc188
KeithL <85144995+KeithL1m@users.noreply.github.com>
2024-04-30 16:09:26 -0700

    add more to Frames and ScreenDisplay

--- Assets/Scripts/UI/OptionsScreen.cs
+++ Assets/Scripts/UI/OptionsScreen.cs
@@ -6,22 +6,48 @@ using TMPro;
 
 public class OptionsScreen : MonoBehaviour
 {
+    // Screen types
     [SerializeField] private List<ScreenType> screenDisplayTypes = new List<ScreenType>();
-
-    public TMP_Text screenLabel;
+    [SerializeField] private TMP_Text screenLabel;
     private int _selectedScreenType;
+
+    // Toggles
+    [SerializeField] private Toggle _vsyncToggle;
+    [SerializeField] private Toggle _particleToggle;
+    [SerializeField] private Toggle _screenshakeToggle;
+
+    //frames
+    private enum frameLimits
+    {
+        noLimit = 0,
+        fps30 = 30,
+        fps60 = 60,
+        fps120 = 120
+    }
+
+    [SerializeField] private List<Frames> frameDisplay = new List<Frames>();
+    private int _selectedLimitDisplay;
+    private int _selectedFPS;
+    [SerializeField] private TMP_Text framesLabel;
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        if(QualitySettings.vSyncCount == 0)
+        {
+            _vsyncToggle.isOn = false;
+        }
+        else
+        {
+            _vsyncToggle.isOn = true;
+        }
     }
-
 
     public void UpdateGraphics()
     {
         if(_selectedScreenType == 0)
         {
-            Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, FullScreenMode.FullScreenWindow);
+            Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, FullScreenMode.MaximizedWindow);
         }
         else if(_selectedScreenType == 1)
         {
@@ -31,6 +57,17 @@ public class OptionsScreen : MonoBehaviour
         {
             Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, FullScreenMode.FullScreenWindow);
         }
+
+        if (_vsyncToggle.isOn)
+        {
+            QualitySettings.vSyncCount = 1;
+        }
+        else
+        {
+            QualitySettings.vSyncCount = 0;
+        }
+
+        //Application.targetFrameRate = frameDisplay[_selectedLimit].frameLimits;
     }
 
     public void ResButtonLeft()
@@ -41,7 +78,7 @@ public class OptionsScreen : MonoBehaviour
             _selectedScreenType = 0;
         }
 
-        UpdateScreen();
+        ResolutionLabelChange();
     }
 
     public void ResButtonRight()
@@ -52,12 +89,39 @@ public class OptionsScreen : MonoBehaviour
             _selectedScreenType = screenDisplayTypes.Count - 1;
         }
 
-        UpdateScreen();
+        ResolutionLabelChange();
     }
 
-    public void UpdateScreen()
+    public void FramesButtonLeft()
+    {
+        _selectedLimitDisplay--;
+        if(_selectedLimitDisplay < 0)
+        {
+            _selectedLimitDisplay = 0;
+        }
+
+        FramesLabelChange();
+    }
+
+    public void FramesButtonRight()
+    {
+        _selectedLimitDisplay++;
+        if (_selectedLimitDisplay > frameDisplay.Count - 1)
+        {
+            _selectedLimitDisplay = frameDisplay.Count - 1;
+        }
+
+        FramesLabelChange();
+    }
+
+    public void ResolutionLabelChange()
     {
         screenLabel.text = screenDisplayTypes[_selectedScreenType].screenTypes;
+    }
+
+    public void FramesLabelChange()
+    {
+        framesLabel.text = frameDisplay[_selectedLimitDisplay].frames;
     }
 }
 
@@ -65,4 +129,10 @@ public class OptionsScreen : MonoBehaviour
 public class ScreenType
 {
     public string screenTypes;
+}
+
+[System.Serializable]
+public class Frames
+{
+    public string frames;
 }
