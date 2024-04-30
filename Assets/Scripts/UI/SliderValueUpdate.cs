400373797c96cbfd0de3e2dac33432a8b0dccbd8
diegobatiz <diegow.batiz@gmail.com>
2024-04-30 14:09:56 -0700

    Merge remote-tracking branch 'origin/KevinHBranch2' into DiegoBranch3

---
+++ Assets/Scripts/UI/SliderValueUpdate.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SliderValueUpdate : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private TMP_Text _sliderText;
+
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        UpdateText(_slider.value);
+        _slider.onValueChanged.AddListener(UpdateText);
+    }
+
+    void UpdateText(float val)
+    {
+        _sliderText.text = _slider.value.ToString();
+    }
+}
