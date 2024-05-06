823086cb9bd51752a28c1728184cc6032bb5e217
diegobatiz <diegow.batiz@gmail.com>
2024-05-06 12:14:54 -0700

    sound manager

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,17 +1,80 @@
 using UnityEngine;
+using System.Collections.Generic;
+
+public enum SoundType
+{
+    Music = 0,
+    SFX = 1
+}
 
 public class AudioManager : MonoBehaviour
 {
     public float MusicVolume;
     public float SoundFXVolume;
 
-    public void PlaySoundEffect()
-    {
+    [SerializeField] private List<AudioSource> _sources = new();
 
+    private void Awake()
+    {
+        GameLoader gm = ServiceLocator.Get<GameLoader>();
+        gm.CallOnComplete(Initialize);
     }
 
-    public void PlayMusic()
+    private void Initialize()
     {
+        
+    }
 
+    //make function for random sound?
+
+    public void PlaySound(AudioClip audio, Vector3 position, float volume = 1f)
+    {
+        //move source to position if not already playing sound
+        AudioSource source = new();
+        for (int i = 0; i < _sources.Count; i++)
+        {
+            if (!_sources[i].isPlaying)
+            {
+                source = _sources[i];
+            }
+            else if (i + 1 == _sources.Count)
+            {
+                AudioSource newSource = new AudioSource();
+                _sources.Add(newSource);
+                source = newSource;
+                break;
+            }
+        }
+
+        source.transform.position = position;
+
+        source.clip = audio;
+
+        source.volume = volume;
+
+        source.Play();
+
+        Destroy(source, 2);
+    }
+
+    public void PlaySound(AudioClip audio, Vector3 position, SoundType type, float volume = 1f)
+    {
+        float newVolume = 1;
+
+        switch(type)
+        {
+            case SoundType.Music:
+                {
+                    newVolume = MusicVolume * volume;
+                    break;
+                }
+            case SoundType.SFX:
+                {
+                    newVolume = SoundFXVolume * volume;
+                    break;
+                }
+        }
+
+        PlaySound(audio, position, newVolume);
     }
 }
--- Assets/Scripts/Tools/GameLoader.cs
+++ Assets/Scripts/Tools/GameLoader.cs
@@ -82,6 +82,7 @@ public class GameLoader : ASyncLoader
         ServiceLocator.Register<ConfigurationManager>(gm.GetComponent<ConfigurationManager>().Initialize());
         ServiceLocator.Register<PlayerManager>(gm.GetComponent<PlayerManager>().Initialize());
         ServiceLocator.Register<LimbManager>(gm.GetComponent<LimbManager>());
+        ServiceLocator.Register<AudioManager>(gm.GetComponent<AudioManager>());
 
         ParticleManager pm = new ParticleManager();
         ServiceLocator.Register<ParticleManager>(pm);
