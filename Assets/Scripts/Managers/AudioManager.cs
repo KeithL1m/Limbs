f1b0d7ffd15d11cbb58fcd9039f599dd3d034f5b
diegobatiz <diegow.batiz@gmail.com>
2024-05-07 14:25:23 -0700

    audio manager stuff

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using System.Collections.Generic;
 
 public enum SoundType
 {
     Music = 0,
-    SFX = 1
+    SFX = 1,
+    None = 2
 }
 
 public class AudioManager : MonoBehaviour
 {
+    [SerializeField] private AudioMixer _audioMixer;
+    [SerializeField] private AudioMixerGroup _masterGroup;
+    [SerializeField] private AudioMixerGroup _sfxGroup;
+    [SerializeField] private AudioMixerGroup _musicGroup;
+
     public float MusicVolume;
     public float SoundFXVolume;
 
@@ -46,35 +53,72 @@ public class AudioManager : MonoBehaviour
             }
         }
 
+        source.outputAudioMixerGroup = _masterGroup;
+
         source.transform.position = position;
 
         source.clip = audio;
 
-        source.volume = volume;
+        source.volume = Mathf.Log10(volume) * 20f;
 
         source.Play();
-
-        Destroy(source, 2);
     }
 
     public void PlaySound(AudioClip audio, Vector3 position, SoundType type, float volume = 1f)
     {
-        float newVolume = 1;
+        //move source to position if not already playing sound
+        AudioSource source = new();
+        for (int i = 0; i < _sources.Count; i++)
+        {
+            if (!_sources[i].isPlaying)
+            {
+                source = _sources[i];
+            }
+            else if (i + 1 == _sources.Count)
+            {
+                AudioSource newSource = new AudioSource();
+                _sources.Add(newSource);
+                source = newSource;
+                break;
+            }
+        }
 
         switch(type)
         {
-            case SoundType.Music:
+            case (SoundType.Music):
                 {
-                    newVolume = MusicVolume * volume;
+                    source.outputAudioMixerGroup = _musicGroup;
                     break;
                 }
-            case SoundType.SFX:
+            case (SoundType.SFX):
                 {
-                    newVolume = SoundFXVolume * volume;
+                    source.outputAudioMixerGroup = _sfxGroup;
                     break;
                 }
+            default: break;
         }
 
-        PlaySound(audio, position, newVolume);
+        source.transform.position = position;
+
+        source.clip = audio;
+
+        source.volume = Mathf.Log10(volume) * 20f;
+
+        source.Play();
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        _audioMixer.SetFloat("Master Volume", Mathf.Log10(volume) * 20f);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        _audioMixer.SetFloat("Music Volume", Mathf.Log10(volume) * 20f);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        _audioMixer.SetFloat("SFX Volume", Mathf.Log10(volume) * 20f);
     }
 }
