cc896bf908729cd93b4f1390397c1cbcd4157e0a
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-09-06 11:23:49 -0700

    get set event for varnet

--- Assets/Scripts/Online/NetworkVariable.cs
+++ Assets/Scripts/Online/NetworkVariable.cs
@@ -1,14 +1,50 @@
 using Unity.Netcode;
 
+public delegate void ChangeValueEvent<T>(T oldValue, T newValue);
+
 public class NetVar<T>
 {
+    private event ChangeValueEvent<T> _changeValueEventFn;
+
     private object _variable;
+
+    public T Value
+    {
+        get
+        {
+            if (_variable is NetworkVariable<T>)
+            {
+                var networkValue = _variable as NetworkVariable<T>;
+                return networkValue.Value;
+            }
+            else
+            {
+                return (T)_variable;
+            }
+        }
+        set
+        {
+            if (_variable is NetworkVariable<T>)
+            {
+                SetValueServerRpc(value);
+            }
+            else
+            {
+                T tempValue = (T)_variable;
+                _variable = value;
+                _changeValueEventFn?.Invoke(tempValue, (T)_variable);
+            }
+        }
+    }
+
 
     public NetVar(T networkVariable, bool isOnline)
     {
         if (isOnline)
         {
             _variable = new NetworkVariable<T>(networkVariable);
+            var networkValue = _variable as NetworkVariable<T>;
+            networkValue.OnValueChanged += OnValueChange;
         }
         else
         {
@@ -16,29 +52,9 @@ public class NetVar<T>
         }
     }
 
-    public void SetValue(T value)
+    public void AddEvent(ChangeValueEvent<T> newEvent)
     {
-        if (_variable is NetworkVariable<T>)
-        {
-            SetValueServerRpc(value);
-        }
-        else
-        {
-            _variable = value;
-        }
-    }
-
-    public T GetValue()
-    {
-        if (_variable is NetworkVariable<T>)
-        {
-            var networkValue = _variable as NetworkVariable<T>;
-            return networkValue.Value;
-        }
-        else
-        {
-            return (T)_variable;
-        }
+        _changeValueEventFn += newEvent;
     }
 
     [ServerRpc]
@@ -46,5 +62,10 @@ public class NetVar<T>
     {
         var networkValue = _variable as NetworkVariable<T>;
         networkValue.Value = value;
+    }
+
+    private void OnValueChange(T oldValue, T newValue)
+    {
+        _changeValueEventFn?.Invoke(oldValue, newValue);
     }
 }
